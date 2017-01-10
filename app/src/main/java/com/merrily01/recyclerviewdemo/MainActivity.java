b91aec8b16eadbe0f6e8cd81139f8136f9ad43c8
merrily01 <merrily01@gmail.com>
2017-01-10 23:19:15 +0800

    网格布局效果

--- app/src/main/java/com/merrily01/recyclerviewdemo/MainActivity.java
+++ app/src/main/java/com/merrily01/recyclerviewdemo/MainActivity.java
@@ -2,6 +2,7 @@ package com.merrily01.recyclerviewdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
@@ -12,9 +13,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView_main);
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
-        linearLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
-        recyclerView.setLayoutManager(linearLayoutManager);
+        //RecyclerView 线性布局效果
+//        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+//        linearLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+        //RecyclerView 网格布局效果
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(MainActivity.this,3,LinearLayoutManager.HORIZONTAL,false);
+        recyclerView.setLayoutManager(gridLayoutManager);
         MyAdapter adapter = new MyAdapter(this);
         recyclerView.setAdapter(adapter);
     }
